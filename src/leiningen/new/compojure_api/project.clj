bcc4d7391f4c8a59a62d19e55121c56deae3f762
Tommi Reiman <tommi@iki.fi>
2014-06-22 09:31:33 +0300

    bump up version

--- src/leiningen/new/compojure_api/project.clj
+++ src/leiningen/new/compojure_api/project.clj
@@ -1,7 +1,7 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [metosin/compojure-api "0.13.0"]
+                 [metosin/compojure-api "0.13.1"]
                  [metosin/ring-http-response "0.4.0"]
                  [metosin/ring-swagger-ui "2.0.16-2"]]
   :ring {:handler {{name}}.handler/app}
