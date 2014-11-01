29e1081a1c29f52c0f76f59fba2a3590235b8166
Tommi Reiman <tommi@iki.fi>
2014-11-01 16:20:41 +0200

    update deps, simple schema app

--- src/leiningen/new/compojure_api/handler.clj
+++ src/leiningen/new/compojure_api/handler.clj
@@ -1,16 +1,18 @@
 (ns {{name}}.handler
   (:require [compojure.api.sweet :refer :all]
-            [ring.util.http-response :refer :all]))
+            [ring.util.http-response :refer :all]
+            [schema.core :as s]))
+
+(s/defschema Message {:message String})
 
 (defapi app
   (swagger-ui)
   (swagger-docs
-    :title "{{nameCamel}} Api"
-    :description "this is {{nameCamel}} Api.")
+    :title "{{nameCamel}} Api")
   (swaggered "math"
-    :description "playing with parameters"
-    (GET* "/plus" []
-      :return Long
-      :query-params [x :- Long, {y :- Long 1}]
-      :summary "x+y (y default to 1)"
-      (ok (+ x y)))))
+    :description "hello api"
+    (GET* "/hello" []
+      :return Message
+      :query-params [name :- String]
+      :summary "hello world"
+      (ok {:message (str "Hello, " name)}))))
--- src/leiningen/new/compojure_api/project.clj
+++ src/leiningen/new/compojure_api/project.clj
@@ -1,11 +1,11 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [metosin/compojure-api "0.16.0"]
-                 [metosin/ring-http-response "0.5.0"]
+                 [metosin/compojure-api "0.16.2"]
+                 [metosin/ring-http-response "0.5.1"]
                  [metosin/ring-swagger-ui "2.0.17"]]
   :ring {:handler {{name}}.handler/app}
   :uberjar-name "server.jar"
   :profiles {:uberjar {:resource-paths ["swagger-ui"]}
              :dev {:dependencies [[javax.servlet/servlet-api "2.5"]]
-                   :plugins [[lein-ring "0.8.11"]]}})
+                   :plugins [[lein-ring "0.8.13"]]}})
