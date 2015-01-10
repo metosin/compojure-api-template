9a51f589acd14bc50c10db98ad682e696513f025
Tommi Reiman <tommi@iki.fi>
2015-01-10 15:43:48 +0200

    update deps

--- resources/leiningen/new/compojure_api/project.clj
+++ resources/leiningen/new/compojure_api/project.clj
@@ -1,11 +1,11 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [metosin/compojure-api "0.16.6"]
+                 [metosin/compojure-api "0.17.0"]
                  [metosin/ring-http-response "0.5.2"]
-                 [metosin/ring-swagger-ui "2.0.17"]]
+                 [metosin/ring-swagger-ui "2.0.24"]]
   :ring {:handler {{name}}.handler/app}
   :uberjar-name "server.jar"
   :profiles {:uberjar {:resource-paths ["swagger-ui"]}
              :dev {:dependencies [[javax.servlet/servlet-api "2.5"]{{{gen-test-deps}}}{{{midje-deps}}}]
-                   :plugins [[lein-ring "0.8.13"]{{{midje-plugs}}}]}})
+                   :plugins [[lein-ring "0.9.0"]{{{midje-plugs}}}]}})
