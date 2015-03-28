7451fcec9a16e51d5dd1692fd68051db0772dbd6
Tommi Reiman <tommi@iki.fi>
2015-03-28 16:39:49 +0200

    bump up versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compojure-api/lein-template "0.9.0"
+(defproject compojure-api/lein-template "0.10.0"
   :description "Compojure-api template"
   :url "https://github.com/metosin/compojure-api-template"
   :license {:name "Eclipse Public License"
--- resources/leiningen/new/compojure_api/project.clj
+++ resources/leiningen/new/compojure_api/project.clj
@@ -1,9 +1,9 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [metosin/compojure-api "0.18.0"]
-                 [metosin/ring-http-response "0.6.0"]
-                 [metosin/ring-swagger-ui "2.0.24"]]
+                 [metosin/compojure-api "0.19.0"]
+                 [metosin/ring-http-response "0.6.1"]
+                 [metosin/ring-swagger-ui "2.1.8-M1"]]
   :ring {:handler {{name}}.handler/app}
   :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]{{{gen-test-deps}}}{{{midje-deps}}}]
