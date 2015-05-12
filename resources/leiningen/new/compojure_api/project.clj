b6f37c1a88e0adbd3f3e49ccb7ca415c615b1e75
Tommi Reiman <tommi@iki.fi>
2015-05-12 20:54:23 +0300

    add clj-time as dependency due to lein-ring... feature

--- resources/leiningen/new/compojure_api/project.clj
+++ resources/leiningen/new/compojure_api/project.clj
@@ -2,6 +2,7 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [metosin/compojure-api "0.20.0"]
+                 [clj-time "0.9.0"] ; required due to bug in lein-ring
                  [metosin/ring-http-response "0.6.1"]]
   :ring {:handler {{name}}.handler/app}
   :uberjar-name "server.jar"
