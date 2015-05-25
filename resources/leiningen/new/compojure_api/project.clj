ef44f413ade37415b9763d33f0a89816d6d7bfd6
Tommi Reiman <tommi@iki.fi>
2015-05-25 16:47:20 +0300

    bump up versions

--- resources/leiningen/new/compojure_api/project.clj
+++ resources/leiningen/new/compojure_api/project.clj
@@ -1,9 +1,8 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [metosin/compojure-api "0.20.3"]
                  [clj-time "0.9.0"] ; required due to bug in lein-ring
-                 [metosin/ring-http-response "0.6.1"]]
+                 [metosin/compojure-api "0.21.0"]]
   :ring {:handler {{name}}.handler/app}
   :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]{{{gen-test-deps}}}{{{midje-deps}}}]
