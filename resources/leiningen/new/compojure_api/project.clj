4043000ace899d98121514b852845c6fe6c75ac8
Tommi Reiman <tommi@iki.fi>
2015-09-01 18:06:32 +0300

    clojure 1.7, compojure-api 0.23.0

--- resources/leiningen/new/compojure_api/project.clj
+++ resources/leiningen/new/compojure_api/project.clj
@@ -1,8 +1,8 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-time "0.9.0"] ; required due to bug in lein-ring
-                 [metosin/compojure-api "0.22.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-time "0.11.0"] ; required due to bug in `lein-ring uberwar`
+                 [metosin/compojure-api "0.23.0"]]
   :ring {:handler {{name}}.handler/app}
   :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]{{{gen-test-deps}}}{{{midje-deps}}}]
