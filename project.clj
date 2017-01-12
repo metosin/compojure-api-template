a9f7336b37bcf1e1d9dd9bf16f8cf6f68bf356e5
Miikka Koskinen <miikka@metosin.fi>
2017-01-12 14:35:00 +0200

    Version 0.16.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compojure-api/lein-template "0.16.7-SNAPSHOT"
+(defproject compojure-api/lein-template "0.16.7"
   :description "Compojure-api template"
   :url "https://github.com/metosin/compojure-api-template"
   :license {:name "Eclipse Public License"
